9c719f7b7fce2c248f16532aab40263fcd413a71
Zeuxis Lo <seekstudio@gmail.com>
2016-07-06 11:35:08 +0800

    add frontend layout and sign in method

--- app/Controllers/HomeController.php
+++ app/Controllers/HomeController.php
@@ -8,7 +8,16 @@ use App\Contracts\Controller;
 class HomeController extends Controller {
 
     public function index(Request $request, Response $response, array $arguments) {
-        return $this->view('index.html');
+        $csrf_tags = $this->csrf->getTokenForHiddenInputTags();
+
+        return $this->view('home/index.html', compact('csrf_tags'));
+    }
+
+    public function signin(Request $request, Response $response, array $arguments) {
+        $username = $request->getParam('username');
+        $password = $request->getParam('password');
+
+        dump($username, $password);
     }
 
 }
--- app/routes.php
+++ app/routes.php
@@ -1,2 +1,3 @@
 <?php
-$app->get('/', 'App\\Controllers\\HomeController:index')->setName('index');
+$app->get('/', 'App\\Controllers\\HomeController:index')->setName('web.home.index');
+$app->post('/signin', 'App\\Controllers\\HomeController:signin')->setName('web.home.signin');
